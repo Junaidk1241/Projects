cfc7f4fb71c01bb776eb907c60fdde66b1fddf99
Junaid Khan <junaid.khan45694@gmail.com>
2023-12-11 13:24:16 +0500

    .

---
+++ Project 4/javascript.js
@@ -0,0 +1,24 @@
+let name=" junaid Khan";
+
+console.log(name);
+
+
+var namee="akbar ali";
+
+console.log(namee);
+
+const nameee="zafar ali";
+// nameee="amjad ali";
+console.log(nameee);
+
+ let numbr=2;
+console.log(numbr);
+
+// junaidKhan Camel case
+// JunaidKhan Pascal case
+// junaid_khan snake case
+// junaid-khan kebab case
+
+
+let number=["Jamshid khan","Wajid Khan","atta khan"];
+console.log(number);
